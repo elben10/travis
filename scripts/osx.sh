e2ec3f2ce435f83410f47a2074ce5c3f9a3e9dfa
Jakob Jul Elben <elbenjakobjul@gmail.com>
2019-02-06 13:38:25 +0100

    Update osx.sh

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -3,4 +3,5 @@ export PATH="${PYENV_ROOT}/bin:${PATH}"
 git clone https://github.com/pyenv/pyenv.git ${PYENV_ROOT}
 pyenv init
 pyenv install ${PYTHON_VERSION}
-pyenv global ${PYTHON_VERSION}
+ls $(pyenv root)/versions/${PYTHON_VERSION}/
+
