93153f3fdbc742e99abc2793db778d2534e0e3c7
Jakob Jul Elben <elbenjakobjul@gmail.com>
2019-02-06 14:58:16 +0100

    Update windows.sh

--- scripts/windows.sh
+++ scripts/windows.sh
@@ -1,1 +1,2 @@
 choco install python --version ${PYTHON_VERSION}
+export PATH="/c/python${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1}/Scripts:/c/python${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1}:${PATH}"
